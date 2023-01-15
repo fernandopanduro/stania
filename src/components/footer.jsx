bd6be6dccccd94bce91cd4d688a1ec76e266afc3
Fernando Panduro <fernandogp2003pro@gmail.com>
2023-01-14 20:37:00 -0700

    Mejorado el footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -6,46 +6,50 @@ import {FiMapPin} from 'react-icons/fi';
 import {BsInstagram} from 'react-icons/bs';
 import {FaFacebookF} from 'react-icons/fa';
 
+import {Fade} from 'react-reveal';  
+
 
 
 export function Footer() {
     return (
-        <footer className={styles.footer}>
-            <div className={`${styles.footerContainer} container`}>
+        <Fade bottom>
+            <footer className={styles.footer}>
+                <div className={`${styles.footerContainer} container`}>
 
-                <div className={styles.listContainer}>
-                    <h4 className={styles.subtitle}><Link to='/'>Stania Clothing</Link></h4>
-                    <ul className={styles.list__socialMedia}>
-                        <li><a href='https://www.instagram.com/stania_mx/' target='_blank'><BsInstagram size={25} /></a></li>
-                        <li><a href='https://www.facebook.com/profile.php?id=100088231982126'><FaFacebookF size={25} /></a></li>
-                        <li><a href='#'><FiMapPin size={25} /></a></li>
-                    </ul>
+                    <div className={styles.listContainer}>
+                        <h4 className={styles.subtitle}><Link to='/'>Stania Clothing</Link></h4>
+                        <ul className={styles.list__socialMedia}>
+                            <li><a href='https://www.instagram.com/stania_mx/' target='_blank'><BsInstagram size={25} /></a></li>
+                            <li><a href='https://www.facebook.com/profile.php?id=100088231982126'><FaFacebookF size={25} /></a></li>
+                            <li><a href='#'><FiMapPin size={25} /></a></li>
+                        </ul>
+                    </div>
+
+                    <div className={styles.listContainer}>
+                        <h4 className={styles.subtitle}>Menu</h4>
+                        <ul className={styles.list}>
+                            <li className={styles.listItem}><a href="#">Inicio</a></li>
+                            <li className={styles.listItem}><a href="#galery">Galeria</a></li>
+                            <li className={styles.listItem}><a href="#about">Nosotros</a></li>
+                            <li className={styles.listItem}><a href="#shopping">Comprar</a></li>
+                        </ul>
+                    </div>
+
+                    <div className={styles.listContainer}>
+                        <h4 className={styles.subtitle}>Terminos</h4>
+                        <ul className={styles.list}>
+                            <li className={styles.listItem}><Link to='/condiciones'>Condiciones</Link></li>
+                            <li className={styles.listItem}><Link to='/privacidad'>Privacidad</Link></li>
+                            <li className={styles.listItem}><Link to='/devoluciones'>Devoluciones</Link></li>
+                        </ul>
+                    </div>
+
+                    <div>
+                        <p className={styles.listItem}>Todos los derechos reservados a <span className={styles.subtitle}><Link to='/'>Stania Clothing</Link></span></p>
+                    </div>
+
                 </div>
-
-                <div className={styles.listContainer}>
-                    <h4 className={styles.subtitle}>Menu</h4>
-                    <ul className={styles.list}>
-                        <li className={styles.listItem}><a href="#">Inicio</a></li>
-                        <li className={styles.listItem}><a href="#galery">Galeria</a></li>
-                        <li className={styles.listItem}><a href="#about">Nosotros</a></li>
-                        <li className={styles.listItem}><a href="#shopping">Comprar</a></li>
-                    </ul>
-                </div>
-
-                <div className={styles.listContainer}>
-                    <h4 className={styles.subtitle}>Terminos</h4>
-                    <ul className={styles.list}>
-                        <li className={styles.listItem}><Link to='/condiciones'>Condiciones</Link></li>
-                        <li className={styles.listItem}><Link to='/privacidad'>Privacidad</Link></li>
-                        <li className={styles.listItem}><Link to='/devoluciones'>Devoluciones</Link></li>
-                    </ul>
-                </div>
-
-                <div>
-                    <p className={styles.listItem}>Todos los derechos reservados a <span className={styles.subtitle}><Link to='/'>Stania Clothing</Link></span></p>
-                </div>
-
-            </div>
-        </footer>
+            </footer>
+        </Fade>
     )
 }
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,36 +5,39 @@ import { Link } from "react-router-dom";
 import {BsInstagram} from 'react-icons/bs';
 
 import { useState } from 'react';
+import { Fade } from 'react-reveal';
 
 export function Header() {
 
     const [isOpen, setIsOpen] = useState(false);
 
     return (
-        <header className={`${styles.header}`}>
+        <Fade top>
+            <header className={`${styles.header}`}>
 
-            <div className='container'>
-                <div className={styles.barsMenu} onClick={() => setIsOpen(!isOpen)}>
-                    <span className={`${styles.line1__barsMenu} ${isOpen && styles.activeline1__barsMenu}`}></span>
-                    <span className={`${styles.line2__barsMenu} ${isOpen && styles.activeline2__barsMenu}`}></span>
-                    <span className={`${styles.line3__barsMenu} ${isOpen && styles.activeline3__barsMenu}`}></span>
-                </div>  
+                <div className='container'>
+                    <div className={styles.barsMenu} onClick={() => setIsOpen(!isOpen)}>
+                        <span className={`${styles.line1__barsMenu} ${isOpen && styles.activeline1__barsMenu}`}></span>
+                        <span className={`${styles.line2__barsMenu} ${isOpen && styles.activeline2__barsMenu}`}></span>
+                        <span className={`${styles.line3__barsMenu} ${isOpen && styles.activeline3__barsMenu}`}></span>
+                    </div>  
 
-                <h1><Link to='/' className={styles.title}>STANIA</Link></h1>
+                    <h1><Link to='/' className={styles.title}>STANIA</Link></h1>
 
-                <a href='https://www.instagram.com/stania_mx/' target='_blank'><BsInstagram size={27} /></a>
-            </div>
+                    <a href='https://www.instagram.com/stania_mx/' target='_blank'><BsInstagram size={27} /></a>
+                </div>
 
-            <div className={`${styles.containerList} ${isOpen && styles.activecontainerList}`}>
-                <nav className={styles.navbar}>
-                    <ul className={styles.list}>
-                        <li className={styles.listItem}><a href="#galery">Galeria</a></li>
-                        <li className={styles.listItem}><a href="#about">Nosotros</a></li>
-                        <li className={styles.listItem}><a href="#Shopping">Comprar</a></li>
-                    </ul>
-                </nav>
-            </div>
+                <div className={`${styles.containerList} ${isOpen && styles.activecontainerList}`}>
+                    <nav className={styles.navbar}>
+                        <ul className={styles.list}>
+                            <li className={styles.listItem}><a href="#galery">Galeria</a></li>
+                            <li className={styles.listItem}><a href="#about">Nosotros</a></li>
+                            <li className={styles.listItem}><a href="#Shopping">Comprar</a></li>
+                        </ul>
+                    </nav>
+                </div>
 
-        </header>
+            </header>
+        </Fade>
     )
 }
